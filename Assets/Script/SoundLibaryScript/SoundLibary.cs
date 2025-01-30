3b02ebecad764af274e386876d48f5fb4f6c61ff
Nattawut <Bank578974@gmail.com>
2025-01-30 19:10:53 +0700

    addBGM

---
+++ Assets/Script/SoundLibaryScript/SoundLibary.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "SpaceShip/SoundLibrary")]
+public class SoundLibary : ScriptableObject
+{
+    public AudioClip bgm1;
+}
